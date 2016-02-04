e9dd368f9a4378a98676954e43156bc93d41ba3f
cojoco <cojoco@users.noreply.github.com>
2016-02-04 13:34:49 -0700

    May have gotten threading to work on lab 3

--- lab3/server.cpp
+++ lab3/server.cpp
@@ -17,6 +17,7 @@
 #include <string>
 #include <pthread.h>
 #include <semaphore.h>
+#include <queue>
 
 #include "cs360utils.h"
 
@@ -28,93 +29,66 @@
 
 using namespace std;
 
-//Global variable so all threads can see it
+//Global variables so all threads can see them
 string basedir;
+sem_t full, empty, mutex;
+
+class socketQueue {
+    std::queue <int> stlQueue;
+    public:
+        //Pushes a socket into the queue
+        void push(int sock) {
+            sem_wait(&empty);
+            sem_wait(&mutex);
+            stlQueue.push(sock);
+            sem_post(&mutex);
+            sem_post(&full);
+        }
+        //Returns a socket and removes it from the queue
+        int pop() {
+            sem_wait(&full);
+            sem_wait(&mutex);
+            int val = stlQueue.front();
+            stlQueue.pop();
+            sem_post(&mutex);
+            sem_post(&empty);
+            return val;
+        }
+} sockqueue;
 
 //Reads from the socket, and writes a response
-void* readFromWriteToSocket(void *socket) {
-    vector<char *> headerLines;
-    char* resource;
-    int hSocket = (long)socket;
+void* readFromWriteToSocket(void *arg) {
+    for (;;) {
+        vector<char *> headerLines;
+        char* resource;
+        int hSocket = sockqueue.pop();
 
-    //Get the headers
-    GetHeaderLines(headerLines, hSocket, false);
+        //Get the headers
+        GetHeaderLines(headerLines, hSocket, false);
 
-    if (headerLines.size() == 0) {
-        //If we don't receive headers, refuse the connection so we don't crash.
-        return 0;
-    }
-
-    resource = strtok(headerLines[0], " ");
-    resource = strtok(NULL, " ");
-
-    string resourceStr = resource;
-
-    string path = basedir + resourceStr;
-    //Get file, if it can be gotten
-    struct stat filestat;
-    
-    char* buffer;
-    //Set content type to html by default
-    string contentType = "text/html";
-    int imageSize = 0;
-
-    bool image = false;
-    bool error = false;
-    //If not found, basically
-    if(stat(path.c_str(), &filestat)) {
-        error = true;
-        //Generate our error message and stick it into the buffer
-        string errorMessage = "<!DOCTYPE html>\n<head></head>\n<body><h1>404</h1><h3>File not found.</h3></body>\n</html>";
-        buffer = (char*)malloc(errorMessage.length()+1);
-        memcpy(buffer, errorMessage.c_str(), errorMessage.length());
-        buffer[errorMessage.length()] = '\0';
-        contentType = "text/html";
-    }
-    //If a file
-    else if(S_ISREG(filestat.st_mode)) {
-        //If a text file
-        if (path.substr(path.size()-3, path.size()-1) == "txt") {
-            FILE *fp = fopen(path.c_str(), "r");
-            buffer = (char *)malloc(filestat.st_size+1);
-            memset(buffer, 0, filestat.st_size);
-            fread(buffer, filestat.st_size, 1, fp);
-            buffer[filestat.st_size] = '\0';
-            contentType = "text/plain";
-            fclose(fp);
+        if (headerLines.size() == 0) {
+            //If we don't receive headers, refuse the connection so we don't crash.
+            return 0;
         }
-        //If an html file
-        else if (path.substr(path.size()-4, path.size()-1) == "html") {
-            FILE *fp = fopen(path.c_str(), "r");
-            buffer = (char *)malloc(filestat.st_size+1);
-            memset(buffer, 0, filestat.st_size);
-            fread(buffer, filestat.st_size, 1, fp);
-            buffer[filestat.st_size] = '\0';
-            fclose(fp);
-        }
-        //If a jpg image
-        else if (path.substr(path.size()-3, path.size()-1) == "jpg") {
-            FILE *fp = fopen(path.c_str(), "r");
-            buffer = (char *)malloc(filestat.st_size);
-            memset(buffer, 0, filestat.st_size);
-            fread(buffer, filestat.st_size, 1, fp);
-            fclose(fp);
-            image = true;
-            imageSize = filestat.st_size;
-            contentType = "image/jpg";
-        }
-        else if (path.substr(path.size()-3, path.size()-1) == "gif") {
-            FILE *fp = fopen(path.c_str(), "r");
-            buffer = (char *)malloc(filestat.st_size);
-            memset(buffer, 0, filestat.st_size);
-            fread(buffer, filestat.st_size, 1, fp);
-            fclose(fp);
-            image = true;
-            imageSize = filestat.st_size;
-            contentType = "image/gif";
-        }
-        else {
-            //404 if not one of these types
+
+        resource = strtok(headerLines[0], " ");
+        resource = strtok(NULL, " ");
+
+        string resourceStr = resource;
+
+        string path = basedir + resourceStr;
+        //Get file, if it can be gotten
+        struct stat filestat;
+        
+        char* buffer;
+        //Set content type to html by default
+        string contentType = "text/html";
+        int imageSize = 0;
+
+        bool image = false;
+        bool error = false;
+        //If not found, basically
+        if(stat(path.c_str(), &filestat)) {
             error = true;
             //Generate our error message and stick it into the buffer
             string errorMessage = "<!DOCTYPE html>\n<head></head>\n<body><h1>404</h1><h3>File not found.</h3></body>\n</html>";
@@ -123,29 +97,20 @@ void* readFromWriteToSocket(void *socket) {
             buffer[errorMessage.length()] = '\0';
             contentType = "text/html";
         }
-    }
-    else if(S_ISDIR(filestat.st_mode)) {
-        int len = 0;
-        DIR *dirp;
-        struct dirent *dp;
-        vector<string> dirList;
-
-        dirp = opendir(path.c_str());
-        while ((dp = readdir(dirp)) != NULL) {
-            char* listing = (char*)malloc(MAX_MSG_SZ);
-            sprintf(listing, "%s", dp->d_name);
-            string temp = listing;
-            dirList.push_back(temp);
-            free(listing);
-        }
-        (void)closedir(dirp);
-        string directoryTempString = "<html>\n<head></head>\n<body>\n";
-        if (resourceStr == "/") resourceStr = "";
-        bool skipDirListing = false;
-        for (int i = 0; i < dirList.size(); i++) {
-            if (dirList[i] == "index.html") {
-                skipDirListing = true;
-                path += "/index.html";
+        //If a file
+        else if(S_ISREG(filestat.st_mode)) {
+            //If a text file
+            if (path.substr(path.size()-3, path.size()-1) == "txt") {
+                FILE *fp = fopen(path.c_str(), "r");
+                buffer = (char *)malloc(filestat.st_size+1);
+                memset(buffer, 0, filestat.st_size);
+                fread(buffer, filestat.st_size, 1, fp);
+                buffer[filestat.st_size] = '\0';
+                contentType = "text/plain";
+                fclose(fp);
+            }
+            //If an html file
+            else if (path.substr(path.size()-4, path.size()-1) == "html") {
                 FILE *fp = fopen(path.c_str(), "r");
                 buffer = (char *)malloc(filestat.st_size+1);
                 memset(buffer, 0, filestat.st_size);
@@ -153,101 +118,163 @@ void* readFromWriteToSocket(void *socket) {
                 buffer[filestat.st_size] = '\0';
                 fclose(fp);
             }
+            //If a jpg image
+            else if (path.substr(path.size()-3, path.size()-1) == "jpg") {
+                FILE *fp = fopen(path.c_str(), "r");
+                buffer = (char *)malloc(filestat.st_size);
+                memset(buffer, 0, filestat.st_size);
+                fread(buffer, filestat.st_size, 1, fp);
+                fclose(fp);
+                image = true;
+                imageSize = filestat.st_size;
+                contentType = "image/jpg";
+            }
+            else if (path.substr(path.size()-3, path.size()-1) == "gif") {
+                FILE *fp = fopen(path.c_str(), "r");
+                buffer = (char *)malloc(filestat.st_size);
+                memset(buffer, 0, filestat.st_size);
+                fread(buffer, filestat.st_size, 1, fp);
+                fclose(fp);
+                image = true;
+                imageSize = filestat.st_size;
+                contentType = "image/gif";
+            }
+            else {
+                //404 if not one of these types
+                error = true;
+                //Generate our error message and stick it into the buffer
+                string errorMessage = "<!DOCTYPE html>\n<head></head>\n<body><h1>404</h1><h3>File not found.</h3></body>\n</html>";
+                buffer = (char*)malloc(errorMessage.length()+1);
+                memcpy(buffer, errorMessage.c_str(), errorMessage.length());
+                buffer[errorMessage.length()] = '\0';
+                contentType = "text/html";
+            }
         }
-        if (!skipDirListing) {
+        else if(S_ISDIR(filestat.st_mode)) {
+            int len = 0;
+            DIR *dirp;
+            struct dirent *dp;
+            vector<string> dirList;
+
+            dirp = opendir(path.c_str());
+            while ((dp = readdir(dirp)) != NULL) {
+                char* listing = (char*)malloc(MAX_MSG_SZ);
+                sprintf(listing, "%s", dp->d_name);
+                string temp = listing;
+                dirList.push_back(temp);
+                free(listing);
+            }
+            (void)closedir(dirp);
+            string directoryTempString = "<html>\n<head></head>\n<body>\n";
+            if (resourceStr == "/") resourceStr = "";
+            bool skipDirListing = false;
             for (int i = 0; i < dirList.size(); i++) {
-                if (dirList[i] != "." && dirList[i] != "..") {
-                    directoryTempString += "<a href=\"" + resourceStr + "/" + dirList[i] + "\">";
-                    directoryTempString += "<h4>" + dirList[i] + "</h4>";
-                    directoryTempString += "</a>\n";
+                if (dirList[i] == "index.html") {
+                    skipDirListing = true;
+                    path += "/index.html";
+                    FILE *fp = fopen(path.c_str(), "r");
+                    buffer = (char *)malloc(filestat.st_size+1);
+                    memset(buffer, 0, filestat.st_size);
+                    fread(buffer, filestat.st_size, 1, fp);
+                    buffer[filestat.st_size] = '\0';
+                    fclose(fp);
                 }
             }
-            directoryTempString += "</body>\n</html>";
-            buffer = (char*)malloc(directoryTempString.length()+1);
-            copy(directoryTempString.begin(), directoryTempString.end(), buffer);
-            buffer[directoryTempString.size()] = '\0';
-            contentType = "text/html";
+            if (!skipDirListing) {
+                for (int i = 0; i < dirList.size(); i++) {
+                    if (dirList[i] != "." && dirList[i] != "..") {
+                        directoryTempString += "<a href=\"" + resourceStr + "/" + dirList[i] + "\">";
+                        directoryTempString += "<h4>" + dirList[i] + "</h4>";
+                        directoryTempString += "</a>\n";
+                    }
+                }
+                directoryTempString += "</body>\n</html>";
+                buffer = (char*)malloc(directoryTempString.length()+1);
+                copy(directoryTempString.begin(), directoryTempString.end(), buffer);
+                buffer[directoryTempString.size()] = '\0';
+                contentType = "text/html";
+            }
         }
-    }
 
-    //Writing stuff
-    char* headers;
-    if (!image && !error) {
-        //Create response headers
-        headers = (char*)malloc(MAX_MSG_SZ);
-        sprintf(headers, "HTTP/1.1 200 OK\nContent-Type: %s\nContent-Length: %d\r\n\r\n", contentType.c_str(), strlen(buffer));
+        //Writing stuff
+        char* headers;
+        if (!image && !error) {
+            //Create response headers
+            headers = (char*)malloc(MAX_MSG_SZ);
+            sprintf(headers, "HTTP/1.1 200 OK\nContent-Type: %s\nContent-Length: %d\r\n\r\n", contentType.c_str(), strlen(buffer));
+            
+            if (write(hSocket, headers, strlen(headers)) != SOCKET_ERROR) {
+            }
+            else {
+                perror("Error writing response headers");
+            }
+
+            if (write(hSocket, buffer, strlen(buffer)) != SOCKET_ERROR) {
+            }
+            else {
+                perror("Error writing response body");
+            }
+            free(buffer);
+        }
+        else if (error) {
+            //Create response headers
+            headers = (char*)malloc(MAX_MSG_SZ);
+            sprintf(headers, "HTTP/1.1 404 Not Found\nContent-Type: %s\nContent-Length: %d\r\n\r\n", contentType.c_str(), strlen(buffer));
+
+            if (write(hSocket, headers, strlen(headers)) != SOCKET_ERROR) {
+            }
+            else {
+                perror("Error writing response headers");
+            }
+
+            if (write(hSocket, buffer, strlen(buffer)) != SOCKET_ERROR) {
+            }
+            else {
+                perror("Error writing response body");
+            }
+            free(buffer);
+        }
+        else {
+            //Create response headers
+            headers = (char*)malloc(MAX_MSG_SZ);
+            sprintf(headers, "HTTP/1.1 200 OK\nContent-Type: %s\nContent-Length: %d\r\n\r\n", contentType.c_str(), imageSize);
+
+            if (write(hSocket, headers, strlen(headers)) != SOCKET_ERROR) {
+            }
+            else {
+                perror("Error writing response headers");
+            }
+
+            if (write(hSocket, buffer, imageSize) != SOCKET_ERROR) {
+            }
+            else {
+                perror("Error writing response body");
+            }
+            free(buffer);
+        }
         
-        if (write(hSocket, headers, strlen(headers)) != SOCKET_ERROR) {
-        }
-        else {
-            perror("Error writing response headers");
-        }
 
-        if (write(hSocket, buffer, strlen(buffer)) != SOCKET_ERROR) {
+        //Free the lines from the headerLines vector
+        for (int i = 0; i < headerLines.size(); i++) {
+            free(headerLines[i]);
         }
-        else {
-            perror("Error writing response body");
-        }
-        free(buffer);
-    }
-    else if (error) {
-        //Create response headers
-        headers = (char*)malloc(MAX_MSG_SZ);
-        sprintf(headers, "HTTP/1.1 404 Not Found\nContent-Type: %s\nContent-Length: %d\r\n\r\n", contentType.c_str(), strlen(buffer));
-
-        if (write(hSocket, headers, strlen(headers)) != SOCKET_ERROR) {
-        }
-        else {
-            perror("Error writing response headers");
-        }
-
-        if (write(hSocket, buffer, strlen(buffer)) != SOCKET_ERROR) {
-        }
-        else {
-            perror("Error writing response body");
-        }
-        free(buffer);
-    }
-    else {
-        //Create response headers
-        headers = (char*)malloc(MAX_MSG_SZ);
-        sprintf(headers, "HTTP/1.1 200 OK\nContent-Type: %s\nContent-Length: %d\r\n\r\n", contentType.c_str(), imageSize);
-
-        if (write(hSocket, headers, strlen(headers)) != SOCKET_ERROR) {
-        }
-        else {
-            perror("Error writing response headers");
-        }
-
-        if (write(hSocket, buffer, imageSize) != SOCKET_ERROR) {
-        }
-        else {
-            perror("Error writing response body");
-        }
-        free(buffer);
-    }
-    
-
-    //Free the lines from the headerLines vector
-    for (int i = 0; i < headerLines.size(); i++) {
-        free(headerLines[i]);
-    }
-    free(headers);
+        free(headers);
 
 
-    /* close socket */
-    #ifdef notdef
-    linger lin;
-    unsigned int y = sizeof(lin);
-    lin.1_onoff=1;
-    lin.1_linger=10;
-    setsockopt(hSocket, SOL_SOCKET, SO_LINGER, &lin, sizeof(lin));
-    shutdown(hSocket, SHUT_RDWR);
-    #endif
-    if(close(hSocket) == SOCKET_ERROR)
-    {
-     perror("\nCould not close socket\n");
-     return 0;
+        /* close socket */
+        #ifdef notdef
+        linger lin;
+        unsigned int y = sizeof(lin);
+        lin.1_onoff=1;
+        lin.1_linger=10;
+        setsockopt(hSocket, SOL_SOCKET, SO_LINGER, &lin, sizeof(lin));
+        shutdown(hSocket, SHUT_RDWR);
+        #endif
+        if(close(hSocket) == SOCKET_ERROR)
+        {
+         perror("\nCould not close socket\n");
+         return 0;
+        }
     }
 }
 
@@ -326,17 +353,27 @@ int main(int argc, char* argv[])
     sigaction(SIGHUP,&signew,&sigold);
     sigaction(SIGPIPE,&signew,&sigold);
 
+    #define MAXQUEUE 20
+
+    //Initialize semaphores with values
+    sem_init(&full, PTHREAD_PROCESS_PRIVATE, 0);
+    sem_init(&empty, PTHREAD_PROCESS_PRIVATE, MAXQUEUE);
+    sem_init(&mutex, PTHREAD_PROCESS_PRIVATE, 1);
+
     pthread_t threads[numThreads];
+    for (int i = 0; i < numThreads; i++) {
+        int rc1;
+
+        if ((rc1=pthread_create( &threads[i], NULL, readFromWriteToSocket, NULL)) != 0) { perror("Thread creation failed"); }
+    }
 
     for(;;)
     {
         /* get the connected socket */
         hSocket=accept(hServerSocket,(struct sockaddr*)&Address,(socklen_t *)&nAddressSize);
         printf("Connection accepted\n");
-
-        int rc1;
-
-        if ((rc1=pthread_create( &threads[0], NULL, readFromWriteToSocket, (void*)hSocket)) != 0) { perror("Thread creation failed"); }
+        sockqueue.push(hSocket);
+        printf("socket pushed\n");
 
     }
 }
