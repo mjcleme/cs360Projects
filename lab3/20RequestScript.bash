af1f98e6647efe8678e080a0be81439a373aec42
cojoco <cojoco@users.noreply.github.com>
2016-02-04 13:50:16 -0700

    Updated 20-request script with correct port number

--- lab3/20RequestScript.bash
+++ lab3/20RequestScript.bash
@@ -2,5 +2,5 @@
 
 for ((i=0; i<20; i++))
 do
-    GET -dUe http://ec2-54-148-248-229.us-west-2.compute.amazonaws.com:4015/test4.jpg &
+    GET -dUe http://ec2-54-148-248-229.us-west-2.compute.amazonaws.com:3000/test4.jpg &
 done
